79c14c3afc0e1b45f0bc31163353080f0ed268d8
dev-chloe <chloekim.4969@gmail.com>
2022-05-26 15:44:01 +0900

    Delete and Update

---
+++ src/components/Nweet.js
@@ -0,0 +1,13 @@
+const Nweet = ({ nweetObj, isOwner }) => (
+  <div>
+    <h4>{nweetObj.text}</h4>
+    {isOwner && (
+      <>
+        <button>Delete Nweet</button>
+        <button>Edit Nweet</button>
+      </>
+    )}
+  </div>
+)
+
+export default Nweet;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,4 @@
+import Nweet from "components/Nweet";
 import { dbService } from "fBase";
 import { addDoc, collection, onSnapshot, query } from "firebase/firestore";
 import { useEffect, useState } from "react";
@@ -56,9 +57,9 @@ const Home = ({ userObj }) => {
         <input type="submit" value="Nweet" />
       </form>
       <div>
-        {nweets.map(nweet => <div key={nweet.id}>
-          <h4>{nweet.text}</h4>
-        </div>)}
+        {nweets.map((nweet) => (
+          <Nweet key={nweet.id} nweetObj={nweet} isOwner={nweet.creatorId === userObj.uid} />
+        ))}
       </div>
     </div>
   )
