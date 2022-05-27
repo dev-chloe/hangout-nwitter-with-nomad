55921d1914649aba4d94aec46fe432b59704ba71
dev-chloe <chloekim.4969@gmail.com>
2022-05-27 14:45:23 +0900

    Preview Images(2)

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,13 @@
 import Nweet from "components/Nweet";
 import { dbService } from "fBase";
 import { addDoc, collection, onSnapshot, orderBy, query } from "firebase/firestore";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState("");
   const [nweets, setNweets] = useState([]);
+  const [attachment, setAttachment] = useState();
+  const fileInput = useRef();
   useEffect(() => {
     const q = query(collection(dbService, "nweets"), orderBy("createdAt", "desc"));
     onSnapshot(q, snapshot => {
@@ -38,10 +40,15 @@ const Home = ({ userObj }) => {
     const theFile = files[0];
     const reader = new FileReader();
     reader.onloadend = (finishedEvent) => {
-      console.log(finishedEvent);
+      const { currentTarget: {result} } = finishedEvent;
+      setAttachment(result);
     }
     reader.readAsDataURL(theFile);
   }
+  const onClearAttachment = () => {
+    setAttachment(null);
+    fileInput.current.value = null;
+  };
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -56,8 +63,16 @@ const Home = ({ userObj }) => {
           type="file"
           accept="image/*"
           onChange={onFileChange}
+          ref={fileInput}
         />
         <input type="submit" value="Nweet" />
+        {
+          attachment && 
+          <div>
+              <img src={attachment} width="50px" height="50px" />
+              <button onClick={onClearAttachment}>Clear</button>
+          </div>
+        }
       </form>
       <div>
         {nweets.map((nweet) => (
