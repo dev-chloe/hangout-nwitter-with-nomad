8a9bccd058c4288bb1c714e9d5abe93109acb803
dev-chloe <chloekim.4969@gmail.com>
2022-06-15 20:13:17 +0900

    refactor: convert readable error messages

--- src/services/AuthService/AuthService.js
+++ src/services/AuthService/AuthService.js
@@ -1,3 +1,5 @@
+import { AuthErrorCodes } from "firebase/auth";
+
 import FirebaseRepository from "repositories/FirebaseRepository";
 
 const signUp = ({ email, password }, setError) => {
@@ -5,21 +7,45 @@ const signUp = ({ email, password }, setError) => {
 
   // TODO: Password rule
 
-  const callbackOnError = (error) => {
-    /*
-    * Firebase 호출시 에러가 나는 경우에 호출되는 함수:
-    * 서비스를 호출하는 컴포넌트의 스테이트를 바꾸는 함수를 주입받아서 호출
-    */
-    setError(error.message);
-  }
-  FirebaseRepository.createNewAccount({ email, password, callbackOnError });
+  FirebaseRepository.createNewAccount({
+    email,
+    password,
+    callbackOnError: (error) => {
+      switch (error.code) {
+        case AuthErrorCodes.EMAIL_EXISTS:
+          setError("이미 사용 중인 이메일입니다.");
+          break;
+        case AuthErrorCodes.WEAK_PASSWORD:
+          setError("비밀번호는 6자리 이상으로 설정해주세요.");
+          break;
+        default:
+          console.error(error);
+          setError("회원가입 과정이 원활하지 않습니다. 잠시 후 재시도 해주세요.");
+      }
+    }
+  });
 }
 
 const login = ({ email, password }, setError) => {
-  const callbackOnError = (error) => {
-    setError(error.message);
-  }
-  FirebaseRepository.signIn({ email, password, callbackOnError })
+  FirebaseRepository.signIn({
+    email,
+    password,
+    callbackOnError: (error) => {
+      switch (error.code) {
+        case AuthErrorCodes.INVALID_PASSWORD:
+        case AuthErrorCodes.USER_DELETED:
+          console.debug(error.code);
+          setError("이메일 또는 비밀번호를 다시 한번 확인해주세요.");
+          break;
+        case AuthErrorCodes.TOO_MANY_ATTEMPTS_TRY_LATER:
+          setError("로그인 시도 횟수가 초과 되었습니다. 잠시 후 재시도 해주세요.");
+          break;
+        default:
+          console.error(error);
+          setError("로그인 과정이 원활하지 않습니다. 잠시 후 재시도 해주세요.");
+      }
+    }
+  })
 }
 
 const logout = () => {
